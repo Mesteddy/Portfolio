024fcb5e02fd8ee894e44a2e20a3df279ea80fe4
Tedros Tsegay <ttsega03@gmail.com>
2023-09-03 16:00:57 -0700

    Made some change

--- Portfolio of Tedros Tsegay_files/buttons.js
+++ Portfolio of Tedros Tsegay_files/buttons.js
@@ -32,7 +32,7 @@ $( "#github" ).click(function() {
   window.open(buttonlink)
 });
 
-$( "#resume" ).attr('data-buttonlink', 'resume.html')
+$( "#resume" ).attr('data-buttonlink', 'Resume.html')
 
 $( "#resume" ).click(function() {
   const buttonlink = $("#resume").data('buttonlink')
